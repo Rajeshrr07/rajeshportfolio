1e5bb23b30792bf3d0fe487267efcaf1195c57c9
Rajesh <devshibasankar@gmail.com>
2023-05-26 15:47:30 +0530

    first commit

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/jamunashri'
+      className='link footer__link'
+      target="_blank"
+    >
+      Rajeshwar Jeyaprakash © 2023
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,104 @@
+const header = {
+  homepage: 'https://jamunashri.netlify.app/',
+  title: 'RJ',
+}
+
+const about = {
+  name: 'Rajeshwar Jeyaprakash',
+  role: 'Front end Developer',
+  description:
+    'A Software Developer with two years of Experience, specializing in Front-end Technologies',
+  resume: 'https://drive.google.com/file/d/1p5mxOuWZVOHyQt23KeCbHoc_qulMOjhO/view?usp=sharing',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/rajeshwar-jeyaprakash/',
+    github: 'https://github.com/Rajeshrr07',
+  },
+}
+
+const projects = [
+  {
+    name: 'Simon-Game',
+    description:
+      'The Simon game is the exciting electronic game of lights and sounds in which players must repeat random sequences of lights by pressing the colored pads in the correct order.',
+    stack: ['JQuery', 'JavaScript'],
+    sourceCode: 'https://github.com/Rajeshrr07/simon_game',
+    livePreview: 'https://rajeshsimongames.netlify.app/',
+  },
+  {
+    name: 'Simon-Game',
+    description:
+      'A simple Simon-Game also using JQuery',
+    stack: ['JQuery'],
+    sourceCode: 'https://github.com/jamunashri/quiz_app',
+    livePreview: 'https://jamunashri-quiz-app.netlify.app/',
+  },
+  {
+    name: 'Number Guessing Game',
+    description:
+      'Build a Number guessing game in React',
+    stack: ['JavaScript',  'React'],
+    sourceCode: 'https://github.com/jamunashri/Number-Guessing-Game',
+    livePreview: 'https://jamunashri-number-guessing-game.netlify.app/',
+  },{
+    name: 'Background Detector',
+    description:
+      'A background Detector application that detects the background color and hide the background color with white',
+    stack: ['JavaScript', 'React', 'Background Detector'],
+    sourceCode: 'https://github.com/jamunashri/background-detector',
+    livePreview: 'https://background-detector.netlify.app/',
+  },{
+    name: 'Indmech Systems',
+    description:
+      'A website for the heating and ventilating company have their own services, contact and about pages',
+    stack: ['JavaScript', 'React', 'Email JS'],
+    sourceCode: 'https://github.com/jamunashri/indemech-systems',
+    livePreview: 'https://indmech.herokuapp.com/'
+  },{
+    name: 'Nithi Tex',
+    description:
+      'A website for the ecommerce site where have their product, category listing, cart, wishlist and payment option',
+      stack: ['JavaScript', 'React', 'Razor Pay', 'EmailJS'],
+    sourceCode: 'https://github.com/jamunashri/NithiTex/tree/main/Nithitex%20code%20final',
+    livePreview: 'http://nithitexadmin.online/'
+  }, {
+    name: 'Video Search App',
+    description: 'A Video Search Application that fetch the list of Video URL JSON file and renders on the UI, If we search something the resulten video will be rendered. Just like a Youtube.',
+    stack: ['JavaScript', 'React', 'HTML5', 'CSS'],
+    sourceCode: 'https://github.com/jamunashri/Video-search-app',
+    livePreview: 'https://video-list-app.netlify.app/'
+  }, {
+    name: 'A Ecommerce Application',
+    description: 'A MERN app which have the admin and customer panel to sell their products',
+    stack:['React', 'NodeJs', 'Razor Pay'],
+    sourceCode: 'https://github.com/jamunashri/ecommerce',
+    livePreview: 'https://dvarnam-couture.netlify.app/'
+  }, {
+    name: 'A Canva Clone App',
+    description: '    A Simple application to represent the Canva UX app :)    ',
+    stack:['React',' HTML',' CSS',' JSX',' ES6',' JS',' Functional Component',' Hook',' npm ','react-draggable'],
+    sourceCode: 'https://github.com/jamunashri/canva-clone',
+    livePreview: 'https://canva-clone1.netlify.app/'
+  }
+]
+
+const skills = [
+  'HTML',
+  'CSS',
+  'JavaScript',
+  "jQuery",
+  'TypeScript',
+  'React',
+  'Redux',
+  'React Context',
+  'SASS',
+  'Material UI',
+  'Git',
+  'ES6',
+  'NPM'
+]
+
+const contact = {
+  email: 'rajeshwarjeyaprakash@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
